016aae6b41ea9ccc6439bca73bb6cfb5c94289ca
ChungTran <chungtv@tadi.io>
2017-12-14 09:31:06 +0700

    Anguler js face data

--- public/js/main/app.js
+++ public/js/main/app.js
@@ -2,4 +2,29 @@ var app = angular.module("app.todos", [])
 
 app.controller("todoController", ['$scope', function ($scope) {
     $scope.appName = "Todo Dashboard "
+    $scope.todos = [
+        {
+            text: "Khởi tạo dự án, include thư viện bootstrap, fontawesome, angularjs, ...",
+            isDone: true
+        }, {
+            text: "Cài đặt Angulerjs app, controller, khởi tạo dữ liệu ban đầu",
+            isDone: true
+        }, {
+            text: "Tạo services gọi api, binding dữ liệu, action, ...",
+            isDone: false
+        }, {
+            text: "Hoàn thành ứng dụng, deploy lên heroku ...",
+            isDone: false
+        }
+    ]
+
+    $scope.createTodo = function () {
+        var todo = {
+            text: $scope.fromData.text,
+            isDone: false
+        }
+
+        $scope.todos.push(todo)
+        $scope.fromData.text = ""
+    }
 }])
